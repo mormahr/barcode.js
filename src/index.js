b7c1597ad9bc4e11a35d8c6b72e0dfcaf927bbb7
Moritz Mahringer <me@moritzmahringer.de>
2017-05-11 22:36:06 +0200

    Add and configure rollup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import {
+	WIDE_SPACE,
+	WIDE_BAR,
+	NARROW_SPACE,
+	NARROW_BAR,
+} from "./Characters"
+export {
+	WIDE_SPACE,
+	WIDE_BAR,
+	NARROW_SPACE,
+	NARROW_BAR,
+}
+
+import {
+	encode39,
+} from "./Code39"
+export {
+	encode39,
+}
