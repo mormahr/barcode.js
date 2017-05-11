574ca67f4af476f24eadd45ed8435e2a24327f31
Moritz Mahringer <me@moritzmahringer.de>
2017-05-11 23:32:47 +0200

    Export HTML renderer

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export { WIDE_SPACE, WIDE_BAR, NARROW_SPACE, NARROW_BAR }
 
 import { encodeCode39 } from "./Code39"
 export { encodeCode39 }
+
+import { renderBarcodeToHTML, generateCSS } from "./HTMLRenderer"
+export { renderBarcodeToHTML, generateCSS }
