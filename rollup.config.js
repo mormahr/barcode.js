2cadd43f26b39534aafc48bf95b8c9f043e5201e
Moritz Mahringer <me@moritzmahringer.de>
2017-05-11 23:24:08 +0200

    Fix use babel plugin in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
+const babel = require("rollup-plugin-babel")
+
 export default {
 	entry: "src/index.js",
 	format: "umd",
 	moduleName: "barcodejs",
-	dest: "dist/barcode.js"
+	dest: "dist/barcode.js",
+	plugins: [
+		babel(),
+	],
 }
