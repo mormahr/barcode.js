49af20920479ecd2772c929e301dbe48623f4a07
Moritz Mahringer <me@moritzmahringer.de>
2017-05-11 21:36:46 +0200

    Use es6 imports / exports

--- src/Characters.js
+++ src/Characters.js
@@ -1,10 +1,4 @@
-"use strict"
-
-const Characters = {
-	WIDE_BAR: "W",
-	NARROW_BAR: "N",
-	WIDE_SPACE: "w",
-	NARROW_SPACE: "n",
-}
-
-module.exports = Characters
+export const WIDE_BAR = "W"
+export const NARROW_BAR = "N"
+export const WIDE_SPACE = "w"
+export const NARROW_SPACE = "n"
