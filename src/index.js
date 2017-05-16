cf7ea9a85827604f7f95ba9fb41e9007cce116bd
Moritz Mahringer <me@moritzmahringer.de>
2017-05-17 00:30:08 +0200

    Rename Codes to Symbologies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { WIDE_SPACE, WIDE_BAR, NARROW_SPACE, NARROW_BAR } from "./Core/Characters"
 export { WIDE_SPACE, WIDE_BAR, NARROW_SPACE, NARROW_BAR }
 
-import { encodeCode39 } from "./Codes/Code39"
+import { encodeCode39 } from "./Symbologies/Code39"
 export { encodeCode39 }
 
 import { renderBarcodeToHTML, generateCSS } from "./Renderers/HTMLRenderer"
